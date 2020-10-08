2d99188967535496a990d8a2bc7a23817c7a5724
Jerel <65048179+jerels@users.noreply.github.com>
2020-10-08 17:20:17 -0500

    added a basic footer

--- app/frontend/src/components/home/Footer.js
+++ app/frontend/src/components/home/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Typography, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        padding: theme.spacing(3, 2),
+        marginTop: 'auto',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    },
+}));
 
 
 const Footer = () => {
+    const classes = useStyles();
+
     return (
-        <h1>Footer</h1>
+        <footer className={classes.footer}>
+            <Container maxWidth="sm">
+                <Typography variant="body1">My sticky footer can be found here.</Typography>
+            </Container>
+        </footer>
     )
 };
 
