8e93befbe538c6ee6eb1cb37b176d6cba0aff462
Jerel <65048179+jerels@users.noreply.github.com>
2020-10-27 16:14:57 -0500

    added userbar

---
+++ app/frontend/src/components/home/UserBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Avatar, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        width: theme.spacing(20),
+        height: theme.spacing(20)
+    }
+}));
+
+const UserBar = ({ username }) => {
+    const classes = useStyles();
+
+    return (
+        <Grid container item xs={4} direction='column' justify="center" alignItems="center">
+            <Avatar className={classes.avatar} alt={username} src={'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'} />
+            <Typography>
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non blandit massa enim nec. Morbi leo urna molestie at elementum eu facilisis sed odio. Amet risus nullam eget felis eget nunc. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium.'
+                </Typography>
+        </Grid>
+    )
+};
+
+
+export default UserBar;
--- app/frontend/src/components/home/UserPage.js
+++ app/frontend/src/components/home/UserPage.js
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
-import { Grid, Paper } from '@material-ui/core';
+import { Avatar, Grid, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
 import { getComics } from '../../store/entities/issues';
 import ComicGrid from './ComicGrid';
+import UserBar from './UserBar';
 
+const useStyles = makeStyles(theme => ({
+    avatar: {
+        width: theme.spacing(20),
+        height: theme.spacing(20)
+    }
+}))
 
 
 const UserPage = ({ userIssues }) => {
     const user = useSelector(state => state.entities.users);
     const issues = useSelector(state => state.entities.issues);
     const dispatch = useDispatch();
+    const classes = useStyles();
     console.log(userIssues);
     console.log(user.issues);
     console.log(issues);
     return (
         <Grid container justify="center" alignItems="center">
-            <Grid item xs={4}></Grid>
+            <UserBar props={user.username} />
             <ComicGrid />
             <Grid item xs={4}></Grid>
         </Grid>
