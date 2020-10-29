0f8a473e97845fb4d25f0898e67de335c25239be
Jerel <65048179+jerels@users.noreply.github.com>
2020-10-29 17:31:22 -0500

    changed the format of the dict sent back in the delete route

--- app/backend/app/api/issues.py
+++ app/backend/app/api/issues.py
@@ -3,7 +3,7 @@ import requests
 from flask import Blueprint, request, make_response
 from hashlib import md5
 from datetime import datetime
-from .issueDict import issueDict
+from .issueDict import issueDict, userCollection
 from ..models import db, UserIssue, User, Issue
 
 issuesRoute = Blueprint('issues', __name__)
@@ -48,11 +48,19 @@ def deleteIssue():
     print('TABLE DELETED')
     user = User.query.filter(User.id == data['userId']).first().toDict()
     if user:
-        userIssues = [issue.toDict()
-                      for issue in user['issues']]
+        userIssues = userCollection(user['issues'])
         issues = [(Issue.query.filter(
             Issue.marvelId == issue['marvelId']).first()).toDict() for issue in userIssues]
+        following = [follow.toDict() for follow in user['followed']]
+        for follow in following:
+            print('!!!!!!!!!', follow)
+            followIssues = userCollection(follow['issues'])
+            followCollection = [(Issue.query.filter(
+                Issue.marvelId == issue['marvelId']).first()).toDict() for issue in followIssues]
+            follow['issues'] = followCollection
+            del follow['followed']
         user['issues'] = issues
+        user['followed'] = following
         return {'user': user}
     else:
         res = make_response({'errors': ['User does not exist']}, 401)
