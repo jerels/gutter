8bb2f8c8651189fcf054a2efb1ffdb976952ae77
Jerel <65048179+jerels@users.noreply.github.com>
2020-10-29 18:24:41 -0500

    ADD FEATURE DONE

--- app/frontend/src/components/home/FollowBar.js
+++ app/frontend/src/components/home/FollowBar.js
@@ -3,15 +3,18 @@ import { Grid, IconButton, Avatar, Typography } from '@material-ui/core';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { useSelector, useDispatch } from 'react-redux';
 import ComicCard from './ComicCard';
-
+import { addToCollection } from '../../store/entities/issues';
 
 const FollowBar = () => {
     const dispatch = useDispatch();
     const followed = useSelector(state => state.entities.users.followed);
+    const userId = useSelector(state => state.session.user);
 
     const handleAdd = marvelId => {
         return async function (e) {
-
+            e.preventDefault();
+            await dispatch(addToCollection(userId, marvelId));
+            return;
         }
     }
 
