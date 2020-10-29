b3d3b0e4b10c73f15c75df3c83927210bb29131c
Jerel <65048179+jerels@users.noreply.github.com>
2020-10-29 17:52:08 -0500

    avatars and add button added to follow bar

--- app/frontend/src/components/home/FollowBar.js
+++ app/frontend/src/components/home/FollowBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Grid, IconButton } from '@material-ui/core';
+import { Grid, IconButton, Avatar, Typography } from '@material-ui/core';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import { useSelector, useDispatch } from 'react-redux';
 import ComicCard from './ComicCard';
 
@@ -8,11 +9,22 @@ const FollowBar = () => {
     const dispatch = useDispatch();
     const followed = useSelector(state => state.entities.users.followed);
 
+    const handleAdd = marvelId => {
+        return async function (e) {
+
+        }
+    }
+
     return (
         <Grid container spacing={2} item xs={4} direction='column'>
+            <Typography>You're friends are reading...</Typography>
             {followed.map(follow => (
                 <Grid key={follow.id} item>
                     <ComicCard cover={follow.issues[0].cover} />
+                    <Avatar alt={follow.username} src={'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'} />
+                    <IconButton onClick={handleAdd(follow.issues[0].marvelId)} color="primary" aria-label="upload picture" component="span">
+                        <AddCircleIcon />
+                    </IconButton>
                 </Grid>
             ))}
         </Grid>
