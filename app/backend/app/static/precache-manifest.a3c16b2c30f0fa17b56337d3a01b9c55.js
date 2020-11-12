27495fe046227b7bf6d7f280fa0680dd7ca9beaf
Jerel <65048179+jerels@users.noreply.github.com>
2020-11-12 10:50:11 -0600

    first heroku push

---
+++ app/backend/app/static/precache-manifest.a3c16b2c30f0fa17b56337d3a01b9c55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f33e83537232595e5f39c3db2f9ae61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3abd7b14df19fb1598",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8b158719edb359ad99f8",
+    "url": "/static/js/2.f063d9f9.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.f063d9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3abd7b14df19fb1598",
+    "url": "/static/js/main.0a88c183.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
